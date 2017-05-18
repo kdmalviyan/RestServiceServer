aec5407bcd0a55b0065997db9a354110670faad3
Kuldeep Singh <kdmalviyan@gmail.com>
2017-05-18 10:53:41 +0530

    Post feature implemented

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # RestServiceServer
 
 http://localhost:8080/api/getUser?id=423638
-http://localhost:8080/api/getUsers/
+http://localhost:8080/api/getUsers
 http://localhost:8080/api/getUsers/ggobi
+
+POST:
+	URL: http://localhost:8080/api/updateUser
+	content-type in header: application.json
+	Payload: 
+		{
+		  "login": "Kuldeep Singh",
+		  "id": 42345638,
+		  "url": "https://api.github.com/orgs/ggobi",
+		  "repos_url": "https://api.github.com/orgs/ggobi/repos",
+		  "events_url": "https://api.github.com/orgs/ggobi/events",
+		  "hooks_url": "https://api.github.com/orgs/ggobi/hooks",
+		  "issues_url": "https://api.github.com/orgs/ggobi/issues",
+		  "members_url": "https://api.github.com/orgs/ggobi/members{/member}",
+		  "public_members_url": "https://api.github.com/orgs/ggobi/public_members{/member}",
+		  "avatar_url": "https://avatars2.githubusercontent.com/u/423638?v=3",
+		  "description": ""
+		}
--- src/main/java/com/kd/example/rest/server/comtroller/RestExampleController.java
+++ src/main/java/com/kd/example/rest/server/comtroller/RestExampleController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +33,11 @@ public class RestExampleController {
 	@RequestMapping(value = "/api/getUser")
 	public GitUser getUserById(@RequestParam("id") int id) {
 		return gitHubService.findUser(id);
+	}
+
+	@RequestMapping(value = "/api/updateUser", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+	public GitUser update(@RequestBody GitUser gitUser) {
+		System.out.println("UPDATED USER:\n" + gitUser);
+		return gitUser;
 	}
 }
